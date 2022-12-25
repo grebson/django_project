03980650820d3c83c83599feb63426dde485fbc8
Tomasz Greber <grebciu@gmail.com>
2022-12-25 12:34:46 +0100

    Authentication implemented

--- core/urls.py
+++ core/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.home_view, name='home'),
+    path('login/', views.login_view, name='login'),
+    path('logout/', views.logout_view, name='logout'),
+    path('register/', views.register_view, name='register'),
 ]
